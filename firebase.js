fa1ab3b86ce6cd1b8173e326dc0b81265bd94b1f
cplacerjth <ctechplace@gmail.com>
2026-08-12 10:25:17 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,19 +4,21 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyB2CON1KM45arVYvu8scs-6uRH8lIVRATA",
+  authDomain: "cjplacerj.firebaseapp.com",
+  projectId: "cjplacerj",
+  storageBucket: "cjplacerj.firebasestorage.app",
+  messagingSenderId: "213770199780",
+  appId: "1:213770199780:web:b959ad70f2ec6c436682f1"
 };
 
+// Inicializa o Firebase (evita duplicar)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
+// Mantém login mesmo fechando o navegador
 setPersistence(auth, browserLocalPersistence);
 
 export { auth, db, storage };
